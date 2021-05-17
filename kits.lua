178766d8ae516f7937b0449717b6047203611da8
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-17 15:50:39 -0500

    Update kits.lua

--- kits.lua
+++ kits.lua
@@ -75,3 +75,17 @@ kitpvp.api.register_kit({
   armor = "leather",
   armor_enchantments = {{protection = 1}, {protection = 1}, {protection = 1}, {protection = 1}}
 })
+
+kitpvp.api.register_kit({
+  name = "spy",
+  items = {
+    item("mcl_tools:sword_stone", {sharpness = 1}) --fighting
+    item("mcl_potions:invisibility", 2) --spying
+    item("mcl_potions:night_vision", 2) --spying
+    item("mcl_books:writable_book", 3) --keeping note of whats happening
+    item("mcl_tools:pick_iron", {efficiency = 1, unbreaking = 1}) --digging pesky blocks to keep spying
+  },
+  default_items = true,
+  armor = "iron",
+  armor_enchantments = {{protection = 1}, {protection = 1}, {protection = 1}, {protection = 1}}
+})

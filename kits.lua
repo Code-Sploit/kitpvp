66a8c8dac73e994489f04703b0762964b5addaaa
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-21 08:32:13 -0500

    Update kits.lua

--- kits.lua
+++ kits.lua
@@ -24,6 +24,22 @@ item = kitpvp.api.get_item_obj
 -- To make the effect converter function a bit shorter
 effect = kitpvp.api.get_effect_obj
 
+-- Define effects to be used
+
+effects = {
+  healing = mcl_potions.healing_func,
+  swiftness = mcl_potions.swiftness_func,
+  leaping = mcl_potions.leaping_func,
+  weakness = mcl_potions.weakness_func,
+  strength = mcl_potions.strength_func,
+  poison = mcl_potions.poison_func,
+  regeneration = mcl_potions.regeneration_func,
+  invisibility = mcl_potions.invisibility_func,
+  water_breating = mcl_potions.water_breathing_func,
+  fire_resistance = mcl_potions.fire_resistance_func,
+  night_vision = mcl_potions.night_vision_func,
+}
+
 kitpvp.api.register_kit({
   name = "tank",
   items = {

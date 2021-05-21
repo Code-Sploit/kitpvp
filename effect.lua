bdf53feff925abefee524b563810738c95b1e8db
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-05-20 21:53:35 -0500

    Create effect.lua

---
+++ effect.lua
@@ -0,0 +1,13 @@
+effects = {
+  healing = mcl_potions.healing_func,
+  swiftness = mcl_potions.swiftness_func,
+  leaping = mcl_potions.leaping_func,
+  weakness = mcl_potions.weakness_func,
+  strength = mcl_potions.strength_func,
+  poison = mcl_potions.poison_func,
+  regeneration = mcl_potions.regeneration_func,
+  invisibility = mcl_potions.invisibility_func,
+  water_breating = mcl_potions.water_breathing_func,
+  fire_resistance = mcl_potions.fire_resistance_func,
+  night_vision = mcl_potions.night_vision_func,
+}
